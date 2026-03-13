aecd21bc018f69fa325c34541fde74b0dbc2b767
alihani2002 <alyhani2002@gmail.com>
2026-03-13 14:49:24 +0200

    Update style of Empolyee and all project

--- PioneerSolution.Data/Migrations/20260313124728_INIT.cs
+++ PioneerSolution.Data/Migrations/20260313124728_INIT.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Microsoft.EntityFrameworkCore.Migrations;
+﻿using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
@@ -26,21 +25,6 @@ namespace PioneerSolution.Data.Migrations
                 });
 
             migrationBuilder.CreateTable(
-                name: "Forms",
-                columns: table => new
-                {
-                    Id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    IsActive = table.Column<bool>(type: "bit", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Forms", x => x.Id);
-                });
-
-            migrationBuilder.CreateTable(
                 name: "PropertyDefinitions",
                 columns: table => new
                 {
@@ -54,51 +38,6 @@ namespace PioneerSolution.Data.Migrations
                 constraints: table =>
                 {
                     table.PrimaryKey("PK_PropertyDefinitions", x => x.Id);
-                });
-
-            migrationBuilder.CreateTable(
-                name: "FormFields",
-                columns: table => new
-                {
-                    Id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    FormId = table.Column<int>(type: "int", nullable: false),
-                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    Label = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    FieldType = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    IsRequired = table.Column<bool>(type: "bit", nullable: false),
-                    OrderSequence = table.Column<int>(type: "int", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_FormFields", x => x.Id);
-                    table.ForeignKey(
-                        name: "FK_FormFields_Forms_FormId",
-                        column: x => x.FormId,
-                        principalTable: "Forms",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                });
-
-            migrationBuilder.CreateTable(
-                name: "FormResponses",
-                columns: table => new
-                {
-                    Id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    FormId = table.Column<int>(type: "int", nullable: false),
-                    SubmittedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
-                    JsonData = table.Column<string>(type: "nvarchar(max)", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_FormResponses", x => x.Id);
-                    table.ForeignKey(
-                        name: "FK_FormResponses_Forms_FormId",
-                        column: x => x.FormId,
-                        principalTable: "Forms",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
                 });
 
             migrationBuilder.CreateTable(
@@ -143,16 +82,6 @@ namespace PioneerSolution.Data.Migrations
                 table: "Employees",
                 column: "Code",
                 unique: true);
-
-            migrationBuilder.CreateIndex(
-                name: "IX_FormFields_FormId",
-                table: "FormFields",
-                column: "FormId");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_FormResponses_FormId",
-                table: "FormResponses",
-                column: "FormId");
         }
 
         /// <inheritdoc />
@@ -162,19 +91,10 @@ namespace PioneerSolution.Data.Migrations
                 name: "EmployeePropertyValues");
 
             migrationBuilder.DropTable(
-                name: "FormFields");
-
-            migrationBuilder.DropTable(
-                name: "FormResponses");
-
-            migrationBuilder.DropTable(
                 name: "Employees");
 
             migrationBuilder.DropTable(
                 name: "PropertyDefinitions");
-
-            migrationBuilder.DropTable(
-                name: "Forms");
         }
     }
 }
